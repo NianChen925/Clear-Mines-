fd35890a110fb0cea45ddc44d5b0a4f8cd26534d
贾佳晨 <1010161943@qq.com>
2015-12-17 20:14:00 +0800

    Initial Commit

---
+++ Clear Mines扫雷/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Clear Mines扫雷
+//
+//  Created by orange on 15/12/17.
+//  Copyright © 2015年 orange. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
